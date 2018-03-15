e9f9661f9c17ef8c792ee952e559ba9024182649
Ryan O'Toole <otoolerj@gmail.com>
2018-03-15 21:11:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -40,6 +40,15 @@ def over?(board)
 end
 
 def winner(board)
+  array = won?(board)
+  if array[0] == "X"
+    return "X"
+  elsif array[0] == "O"
+    return "O"
+  else
+    return nil 
+
+
   if won?(board) board[array[0]] == "X"
     return"X"
   elsif won?(board) board[array[0]] == "O"
