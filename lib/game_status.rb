9f65a4538ee228cea0d2a80bee4850581c79d4fd
Ryan O'Toole <otoolerj@gmail.com>
2018-03-15 20:48:49 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -34,6 +34,5 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) == true || full?(board) == true || draw?(board) == true
-  end
+  won?(board) == true || full?(board) == true || draw?(board) == true
 end
