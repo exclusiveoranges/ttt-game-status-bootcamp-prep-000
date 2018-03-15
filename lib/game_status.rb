eaff73c12985d9bfccbc7bc7dacbb821d3815e52
Ryan O'Toole <otoolerj@gmail.com>
2018-03-15 20:12:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,36 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [0,4,8]
+]
+
+def won?(board)
+    WIN_COMBINATIONS.detect do |array|
+       board[array[0]] == board[array[1]] && board[array[2]] == "X" && board[array[0]] == board[array[2]] ||
+       board[array[0]] == board[array[1]] && board[array[2]] == "O" && board[array[0]] == board[array[2]]
+
+    end
+end
+
+def full?(board)
+  board.all? { |element| element == "X" || element == "O" }
+end
+
+def draw?(board)
+  if full?(board) == true && won?(board) == false
+    return true
+  else return false
+  end
+end
+
+def over?
+
+end
